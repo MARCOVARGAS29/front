7f18287f4322d749e0b324827f44eb8f3f4bd8f6
MARCOVARGAS29 <jhean.vargas@ucb.edu.bo>
2024-12-19 02:41:12 -0400

    finalfff

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  
   private userSubject = new BehaviorSubject<any>(null);
   user$ = this.userSubject.asObservable(); // Suscribirse al usuario actual
 
-  private apiUrl = 'http://localhost:3000/usuario'; // Tu API base
+  private apiUrl = 'https://backe-585f.onrender.com'; // URL base del backend
 
   constructor(private http: HttpClient) {}
 
   // Método para iniciar sesión
   login(usuario: { nombre: string, contrasena: string, tipousuario: string }): Observable<any> {
-    return this.http.post('http://localhost:3000/usuario/login',usuario);
+    return this.http.post(`${this.apiUrl}/usuario/login`, usuario);
   }
 
   // Getter para el usuario actual
@@ -31,20 +30,17 @@ export class AuthService {
   }
   
   // Método para registrar un nuevo usuario
-  registrarusuario(usuario: { nombre: string; contrasena: string; email: string; numcontacto: number; tipousuario: string }): Observable<any> 
-  {
-    return this.http.post('http://localhost:3000/usuario',usuario);
+  registrarusuario(usuario: { nombre: string; contrasena: string; email: string; numcontacto: number; tipousuario: string }): Observable<any> {
+    return this.http.post(`${this.apiUrl}/usuario`, usuario);
   }
-  //Metodo para registrar un nuevo evento
-  registrarevento(evento: { nombre: string; tipo_evento: string; descripcion: string; id_usuario: string; id_espacio:string; fecha_evento: string; capacidad_personas: number; hora_inicio:number; hora_fin:number; tipo_pago:string; img_evento:string }):Observable<any>
-  {
-    return this.http.post('http://localhost:3000/evento',evento);
+
+  // Método para registrar un nuevo evento
+  registrarevento(evento: { nombre: string; tipo_evento: string; descripcion: string; id_usuario: string; id_espacio: string; fecha_evento: string; capacidad_personas: number; hora_inicio: number; hora_fin: number; tipo_pago: string; img_evento: string }): Observable<any> {
+    return this.http.post(`${this.apiUrl}/evento`, evento);
   }
-  
-  //Metodo para registrar un nuevo espacio
-  registrarespacio(espacio: { nombre: string; ubicacion:string; costo: number }):Observable<any>
-  {
-    return this.http.post('http://localhost:3000/espacio',espacio);
+
+  // Método para registrar un nuevo espacio
+  registrarespacio(espacio: { nombre: string; ubicacion: string; costo: number }): Observable<any> {
+    return this.http.post(`${this.apiUrl}/espacio`, espacio);
   }
 }
-
